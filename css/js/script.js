4635caff32a8b6e30f249e851587bb6aa270336e
alessandro-ricci-boolean <cqfp.official@gmail.com>
2023-11-20 16:06:15 +0100

    created scaffolding

---
+++ css/js/script.js
@@ -0,0 +1,12 @@
+let km = prompt(Inserire kilometraggio);
+
+let age = prompt(Inserire età);
+
+let price = km * 0.21;
+
+if age < 18 {
+    price == price - price * 0.20
+}
+if age > 64 {
+    price == price - price * 0.40
+}
