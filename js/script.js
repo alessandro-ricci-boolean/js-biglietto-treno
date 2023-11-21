bc1ef2ff0c75cecb436d726855ac8c8d38c71001
alessandro-ricci-boolean <cqfp.official@gmail.com>
2023-11-21 17:34:54 +0100

    added toFixed feature

--- js/script.js
+++ js/script.js
@@ -2,15 +2,15 @@ let km = prompt("Inserire kilometraggio");
 
 let age = prompt("Inserire età");
 
-let price = km * 0.21;
+let price = km * 0.21 .toFixed(2);
 console.log(price)
 if (age < 18) {
-    let sconto = price * 0.20
-    price = price - sconto
+    let discount = price * 0.20 .toFixed(2);
+    price = price - discount
 }
 else if (age > 64) {
-    let sconto = price * 0.40
-    price = price - sconto
+    let discount = price * 0.40 .toFixed(2);
+    price = price - discount
 }
 else {} 
-console.log(price)
+console.log(price) 
